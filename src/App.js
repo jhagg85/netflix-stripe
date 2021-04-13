738357a15c65a7bc1e4c3806e00eb29645dc8d52
jhagg85 <jhaggy85@gmail.com>
2021-04-13 14:00:25 +0800

    finalize netflix frontend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 import Nav from "./Nav";
 import HomeScreen from "./HomeScreen";
-import Row from "./Row";
-
-import requests from "./Requests";
 
 import "./App.css";
 
@@ -13,19 +10,6 @@ function App() {
       <Nav />
 
       <HomeScreen />
-
-      <Row
-        title="NETFLIX ORIGINALS"
-        fetchURL={requests.fetchNetFlixOriignals}
-        isLargeRow
-      />
-      <Row title="Trending Now" fetchURL={requests.fetchNetFlixOriignals} />
-      <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
-      <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
-      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
-      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
-      <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies} />
-      <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
     </div>
   );
 }
--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Banner from "./Banner";
 import Nav from "./Nav";
+import Row from "./Row";
 
 import "./HomeScreen.css";
+import requests from "./Requests";
 
 function HomeScreen() {
   return (
@@ -11,7 +13,18 @@ function HomeScreen() {
 
       <Banner />
 
-      {/* Rows */}
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchURL={requests.fetchNetFlixOriignals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchURL={requests.fetchNetFlixOriignals} />
+      <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
     </div>
   );
 }
